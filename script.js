5166155092bc9b1648115db8fef6944991dc1636
Nele Hirsch <nele@ebildungslabor.de>
2020-12-13 00:37:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const sounds = ['Gleich geht es los', 'Das war toll', 'Leider falsch', 'Bitte kurz Geduld', 'Entspannt Euch']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
